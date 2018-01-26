b5c41345d4a183e41ebaa03b26c3eee15c0d9422
Ken Albers <ken.albers@gmail.com>
2018-01-25 22:15:39 -0500

    Updates to content and styles on exhibit explore and show pages.

--- exhibit-builder/exhibits/browse.php
+++ exhibit-builder/exhibits/browse.php
@@ -2,11 +2,13 @@
 $title = __('Browse Exhibits');
 echo head(array('title' => $title, 'bodyclass' => 'exhibits browse'));
 ?>
-<p class="orientation large-10 large-offset-1 columns">Explore Digital Chicago!<span class="show-for-medium"> Use the tags below to filter our faculty projects.</span></p>
+<p class="orientation large-10 large-offset-1 columns">Explore Digital Chicago.<span class="show-for-medium"> Use the tags below to filter our faculty projects.</span></p>
 
 <div class="tag-cloud align-spaced show-for-medium column">
 <?php $tags = get_records('Tag', array('type'=>'exhibit' ),0); ?>
-<?php echo tag_cloud($tags, 'exhibits/browse'); ?>
+<?php 
+    sort($tags);
+    echo tag_cloud($tags, 'exhibits/browse'); ?>
 </div>
 <div class="exhibits">
     <?php if (count($exhibits) > 0): ?>
